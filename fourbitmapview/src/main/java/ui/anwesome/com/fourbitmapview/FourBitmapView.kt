3be20824c232b9056da18ff8f7316e126dc2f327
Anwesh43 <anweshthecool0@gmail.com>
2018-01-25 12:10:00 +0530

    adding Animator

--- fourbitmapview/src/main/java/ui/anwesome/com/fourbitmapview/FourBitmapView.kt
+++ fourbitmapview/src/main/java/ui/anwesome/com/fourbitmapview/FourBitmapView.kt
@@ -95,4 +95,29 @@ class FourBitmapView(ctx:Context,var bitmap:Bitmap):View(ctx) {
             }
         }
     }
+    data class Animator(var view:FourBitmapView,var animated:Boolean = false) {
+        fun animate(updatecb:()->Unit) {
+            if(animated) {
+                updatecb()
+                try {
+                    Thread.sleep(50)
+                    view.invalidate()
+                }
+                catch(ex:Exception) {
+
+                }
+            }
+        }
+        fun start() {
+            if(!animated) {
+                animated = true
+                view.postInvalidate()
+            }
+        }
+        fun stop() {
+            if(animated) {
+                animated = false
+            }
+        }
+    }
 }
