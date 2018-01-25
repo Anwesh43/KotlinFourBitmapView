aa48fb73017cf0c170cf10761679dbf7a4770ab2
Anwesh43 <anweshthecool0@gmail.com>
2018-01-25 13:35:28 +0530

    adding FourBitmapView in activity

--- app/src/main/java/ui/anwesome/com/kotlinfourbitmapview/MainActivity.kt
+++ app/src/main/java/ui/anwesome/com/kotlinfourbitmapview/MainActivity.kt
@@ -1,12 +1,14 @@
 package ui.anwesome.com.kotlinfourbitmapview
 
+import android.graphics.BitmapFactory
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import ui.anwesome.com.fourbitmapview.FourBitmapView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        FourBitmapView.create(this,BitmapFactory.decodeResource(resources,R.drawable.nature_more))
     }
 }
